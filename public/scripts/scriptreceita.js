8f4125a181016523e7fc4e5bf3b48af69d4bf7fb
raphaelfasilva <raphaelfasilva@gmail.com>
2020-09-07 23:00:51 -0300

    modificações 7/09/20

--- public/scripts/scriptreceita.js
+++ public/scripts/scriptreceita.js
@@ -1,23 +1,3 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const modal = document.querySelector('.modal')
-const receitas = document.querySelectorAll('.receita')
-const modalImagem = document.querySelector(".modal-imagem img")
-const modalTitulo = document.querySelector(".modal-titulo")
-const modalAutor = document.querySelector(".modal-autor")
-for (let receita of receitas) {
-    const cardImage = receita.querySelector(".imagem-receita img")
-    const cardTitle = receita.querySelector(".titulo-receita")
-    const cardAutor = receita.querySelector(".autor-receita")
-    receita.addEventListener("click", function() {
-        modalOverlay.classList.add("active")
-        modalImagem.src = cardImage.getAttribute("src")
-        modalTitulo.textContent = cardTitle.textContent
-        modalAutor.textContent = cardAutor.textContent
-    })
-}
-document.querySelector(".close-modal").addEventListener("click", function() {
-    modalOverlay.classList.remove("active")
-})
 const ingredients = document.querySelector('.recipe-container-ingredients-content')
 const preparation = document.querySelector('.recipe-container-preparation-content')
 const information = document.querySelector('.recipe-container-information-content')
